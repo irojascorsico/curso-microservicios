425996c079d2c15a964fe38de79c838d608eda0b
Rojas Córsico Ivana Soledad <irojas@gmail.com>
2025-07-28 00:44:25 -0300

    product microservice

---
+++ microservices/product-microservice/src/main/java/com/irojas/microservices/product_microservice/exceptions/CategoryException.java
@@ -0,0 +1,10 @@
+package com.irojas.microservices.product_microservice.exceptions;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class CategoryException extends RuntimeException {
+    private final String message;
+}
---
+++ microservices/product-microservice/src/main/java/com/irojas/microservices/product_microservice/product/ProductQuantityRequest.java
@@ -0,0 +1,12 @@
+package com.irojas.microservices.product_microservice.product;
+
+import jakarta.validation.constraints.NotNull;
+
+public record ProductQuantityRequest(
+    @NotNull(message = "Product ID cannot be null")
+    Integer productId,
+    @NotNull(message = "Quantity cannot be null")
+    Integer quantity
+) {
+
+}
